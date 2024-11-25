74c2c77f67296ff0e1a8f0ee2ba2529bcc3e858a
Gideon Bawa <kieriantirian@gmail.com>
2024-11-25 18:57:06 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    ignoreDuringBuilds: true, // Skip ESLint checks during builds
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
